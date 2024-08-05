d0da2502580cb2bbcb1ad95eae754d201ab0ddac
Moussa <mossam808@gmail.com>
2024-08-05 15:29:56 +0300

    problem

--- B. Prime checking.py
+++ B. Prime checking.py
@@ -1,4 +1,4 @@
-
+import math
 n=int(input())
 if n<=1:
     print("NO")
@@ -6,7 +6,7 @@ elif n==2:
     print("YES")
 else:
     y=0
-    for i in range(2,n-1):
+    for i in range(2,int(math.sqrt(n))+1):
 
         if n%i==0 :
             y=1
@@ -17,21 +17,3 @@ else:
         print('YES')
 
     
-# import math
-
-# n = int(input())
-
-# if n <= 1:
-#     print('NO')
-# elif n == 2:
-#     print('YES')
-# else:
-#     y = 0
-#     for i in range(2, int(math.sqrt(n)) + 1):
-#         if n % i == 0:
-#             y = 1
-#             break
-#     if y == 1:
-#         print('NO')
-#     else:
-#         print('YES')
