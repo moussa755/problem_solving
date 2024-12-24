2ea088b99921bf1dbcb5bf5ca3c1fc3a8afb322e
Moussa <mossam808@gmail.com>
2024-12-24 15:02:11 +0200

    problem solving

--- S. Sum of Consecutive Odd Numbers.py
+++ S. Sum of Consecutive Odd Numbers.py
@@ -1,14 +1,9 @@
-t=int(input())
-for i in range(t):
-    x,y = map(int,input().split())
-    max=max(x,y)
-    min=min(x,y)
-    n=0
-    for i in range(min+1,max):
-        
-        if i % 2 !=0:
-            n=+i   
-            
-    print(n)
+num =int(input())
 
-
+for i in range(num):
+    x,y=map(int,input().split())
+    if x > y :
+        x, y =y, x
+    sum_odd=sum(n for n in range(x+1,y) if n%2!=0)
+    print(sum_odd)
+    
